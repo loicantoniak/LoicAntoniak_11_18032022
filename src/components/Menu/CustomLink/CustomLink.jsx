2a66a0c946e778f7096faaef6379e4dff3d4a8eb
Antoniak Loïc <loic.antoniak@gmail.com>
2022-03-19 22:09:10 +0100

    noMatch page done

--- src/components/Menu/CustomLink/CustomLink.jsx
+++ src/components/Menu/CustomLink/CustomLink.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link, useResolvedPath, useMatch } from "react-router-dom";
+
+CustomLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
 
 export default function CustomLink({ children, to, ...props }) {
   let resolved = useResolvedPath(to);
--- src/pages/NoMatch/NoMatch.jsx
+++ src/pages/NoMatch/NoMatch.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nomatch.scss"
 
 export default function NoMatch() {
   return (
-    <div>Error404</div>
-  )
+    <div className="noMatch_container">
+      <h1>404</h1>
+      <h2>Oups ! La page que vous demandez n'existe pas.</h2>
+      <Link to={"/"}>Retourner sur la page d’accueil</Link>
+    </div>
+  );
 }
